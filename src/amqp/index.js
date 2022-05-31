074af21958e539212e035b858973631c75048a6c
Warayout (Tum) <warayout.hem@ascendcorp.com>
2022-05-31 15:46:47 +0700

    import env

--- src/amqp/index.js
+++ src/amqp/index.js
@@ -1,10 +1,11 @@
 const amqp = require("amqplib/callback_api");
 const fs = require("fs");
+require("dotenv/config");
 const normalizedPath = require("path").join(__dirname, "/consumer");
 let amqpConnection;
 
 exports.amqpConnection = () => amqpConnection;
-
+console.log('process.env.AMQP_UR', process.env.AMQP_URL)
 exports.init = () => {
     // "amqp://localhost:5672" 
   amqp.connect(process.env.AMQP_URL, async (error, connection) => {
--- src/redis/index.js
+++ src/redis/index.js
@@ -1,16 +1,10 @@
 const Redis = require('ioredis')
-
+require("dotenv/config");
 const client = new Redis({
     port: process.env.REDIS_PORT, // Redis port
     host: process.env.REDIS_URL, // Redis host
     username: process.env.REDIS_USER, // needs Redis >= 6
     password: process.env.REDIS_PASSWORD,
     db: 0, // Defaults to 0
-})
-
-client.on("connection", (stream) => {
-    console.log("redis is connected")
-}).on("error", () => {
-    console.log("redis is error:", error)
 })
 module.exports = client
