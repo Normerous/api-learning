48e76e810a23a20384d3af62d11ade515f06b90a
Warayout (Tum) <warayout.hem@ascendcorp.com>
2022-05-31 15:31:47 +0700

    fix error redis

--- src/redis/index.js
+++ src/redis/index.js
@@ -1,10 +1,16 @@
 const Redis = require('ioredis')
 
-const client = new Redis('redis://localhost:6379')
+const client = new Redis({
+    port: process.env.REDIS_PORT, // Redis port
+    host: process.env.REDIS_URL, // Redis host
+    username: process.env.REDIS_USER, // needs Redis >= 6
+    password: process.env.REDIS_PASSWORD,
+    db: 0, // Defaults to 0
+})
 
-// client.on("connection", (stream) => {
-//     console.log("redis://localhost:6379 is connected")
-// }).on("error", () => {
-//     console.log("error redis: ", error)
-// }
+client.on("connection", (stream) => {
+    console.log("redis is connected")
+}).on("error", () => {
+    console.log("redis is error:", error)
+})
 module.exports = client
